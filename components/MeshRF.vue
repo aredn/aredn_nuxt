9f4f37e779a89a97b2fe002835ea53ead025bf88
ab7pa <ab7pa.radio@gmail.com>
2021-05-07 09:22:19 -0700

    Fix Issue with Undefined Values
    Fix an issue with undefined values.

--- components/MeshRF.vue
+++ components/MeshRF.vue
@@ -14,10 +14,10 @@
           <p class="label">Frequency (MHz):</p>
         </v-col>
         <v-col>
-          <p class="mb-0">{{ info.ssid === nil ? "Disabled" : info.ssid }}</p>
-          <p class="mb-0">{{ info.channel === nil ? "Disabled" : info.channel }}</p>
-          <p class="mb-0">{{ info.chanbw === nil ? "Disabled" : info.chanbw }}</p>
-          <p class="mb-0">{{ info.frequency === nil ? "Disabled" : info.frequency }}</p>
+          <p class="mb-0">{{ info.ssid === undefined ? "Disabled" : info.ssid }}</p>
+          <p class="mb-0">{{ info.channel === undefined ? "Disabled" : info.channel }}</p>
+          <p class="mb-0">{{ info.channel === undefined ? "Disabled" : info.chanbw }}</p>
+          <p class="mb-0">{{ info.frequency === undefined ? "Disabled" : info.frequency }}</p>
         </v-col>
       </v-row>
     </v-card-text>
