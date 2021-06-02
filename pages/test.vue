0771ab4a889f7524410da01713ae7a0f77d807e3
Darryl Quinn <dman776@gmail.com>
2021-06-01 20:53:12 -0500

    add test page

--- pages/test.vue
+++ pages/test.vue
@@ -9,11 +9,18 @@
       <p>hash: {{ genBasicAuthHash }}</p>
       <p>header: {{ genHeader }}</p>
       <hr />
+      <!-- <p>{{ this.getHostsFiltered("K5DLQ-HAP-1") }}</p> -->
+      <!-- <nodes-traceroute node="KD0VWH-HAPAC01" /> -->
+      <nodes-traceroute node="AC0EC-InternetHub" />
     </div>
   </div>
 </template>
 
 <script>
+import { mapMutations, mapGetters } from "vuex";
+
+const dataService = process.env.apiROOT + "/api?mesh=allhosts";
+
 export default {
   data() {
     return {
@@ -28,6 +35,25 @@ export default {
     genHeader() {
       return "Basic " + this.genBasicAuthHash;
     },
+  },
+  methods: {
+    ...mapMutations({
+      addHosts: "hosts/add",
+    }),
+    ...mapGetters({
+      getHosts: "hosts/getHosts",
+      getHost: "hosts/getHost",
+      getHostsFiltered: "hosts/getHostsFiltered",
+    }),
+  },
+  async fetch() {
+    try {
+      this.info = await fetch(dataService).then((res) => res.json());
+      this.addHosts(this.info.pages.mesh.allhosts);
+      $nuxt.$root.$loading.finish();
+    } catch (error) {
+      console.log("ERROR: " + error);
+    }
   },
 };
 </script>
