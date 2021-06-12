1b73a8c13e6bfb8c9f280c8d9cb0208d30ed09d9
ab7pa <ab7pa.radio@gmail.com>
2021-06-12 13:52:24 -0700

    newui: services for non-RF nodes
    This depends on arednPR#120 for the getRemRFIP function that sets IP for 
    non-RF linked nodes. This puts all Current Neighbor information on 
    single rows rather than expansion panels.

--- components/nodes/neighbors.vue
+++ components/nodes/neighbors.vue
@@ -1,35 +1,28 @@
 <template>
   <div>
     <v-container multiple>
-      <v-row class="accent" justify="center" no-gutters>
-        <v-col cols="5">Node</v-col>
-        <v-col cols="2">IP</v-col>
+      <v-row class="accent" no-gutters>
+        <v-col cols="3">Node</v-col>
         <v-col cols="1">Type</v-col>
         <v-col cols="1">LQ</v-col>
         <v-col cols="1">NLQ</v-col>
         <v-col cols="2">Est.Thru</v-col>
+        <v-col cols="4">Services</v-col>
       </v-row>
     </v-container>
 
-    <v-expansion-panels>
-      <v-expansion-panel v-for="(node, ip) in currentNeighbors" :key="ip">
-        <v-expansion-panel-header>
-          <v-container>
-            <v-row align="start">
-              <v-col cols="5">{{ node.hostname }}</v-col>
-              <v-col cols="2">{{ ip }}</v-col>
-              <v-col cols="1">{{ node.linkType }}</v-col>
-              <v-col cols="1">{{ (node.linkQuality * 100).toFixed(0) }}%</v-col>
-              <v-col cols="1">{{ (node.neighborLinkQuality * 100).toFixed(0) }}%</v-col>
-              <v-col cols="2">{{ node.expected_throughput }}MB/s</v-col>
-            </v-row>
-          </v-container>
-        </v-expansion-panel-header>
-        <v-expansion-panel-content>
+    <v-container v-for="(node, ip) in currentNeighbors" :key="ip">
+      <v-row align="start">
+        <v-col cols="3">{{ node.hostname }}</v-col>
+        <v-col cols="1">{{ node.linkType }}</v-col>
+        <v-col cols="1">{{ (node.linkQuality * 100).toFixed(0) }}%</v-col>
+        <v-col cols="1">{{ (node.neighborLinkQuality * 100).toFixed(0) }}%</v-col>
+        <v-col cols="1">{{ node.expected_throughput }}MB/s</v-col>
+        <v-col cols="5">
           <nodes-servicechips :ip="ip" />
-        </v-expansion-panel-content>
-      </v-expansion-panel>
-    </v-expansion-panels>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
